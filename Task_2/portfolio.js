228ce836e0acf76b6b5b391a94a3a85d43ad47c2
Aryan Gupta <141831855+ag-dev-ml@users.noreply.github.com>
2023-08-24 16:13:40 +0530

    Add files via upload

---
+++ Task_2/portfolio.js
@@ -0,0 +1,22 @@
+// // var typed=new Typed(".multiple-text",{
+// //     strings: ["Developer","Programmer","Machine Learning",
+// //     "Freelancer"],
+// //     typeSpeed:100,
+// //     backSpeed:100,
+// //     backDelay:1000,
+// //     loop:true
+// // })
+
+// const toggleIcon=document.querySelector(".toggle-icon");
+// toggleIcon.addEventListener("click",()=> {
+//     toggleIcon.classList.toggle("bx-sun");
+//     document.querySelector("body").style.background="black";
+//     document.getElementsByClassNames('home').style.color="white";
+// });
+
+
+// window.onscroll = () => {
+//     let header=document.getElementsByClassName(".header");
+//     header.classList.toggle("sticky",window.scrollY>100);
+
+// };
